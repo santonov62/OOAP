b5508ce7155972b8cfaac0a524536b4b0a43e67d
santonov62 <flex62ryz@ya.ru>
2018-09-03 17:36:22 +0300

    Dog door

--- DogDoor/src/com/santonov/Remote.java
+++ DogDoor/src/com/santonov/Remote.java
@@ -1,5 +1,8 @@
 package com.santonov;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Remote {
     private DogDoor door;
 
@@ -14,5 +17,14 @@ public class Remote {
         } else {
             door.open();
         }
+
+        final Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                door.close();
+                timer.cancel();
+            }
+        }, 5000);
     }
 }
